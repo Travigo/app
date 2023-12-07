a9e951d8683c07723ef212a1a02838c638141d4d
Aaron Claydon <claydon.aaron@gmail.com>
2023-12-07 23:04:25 +0000

    Lookup departures

--- lib/stopviewscreen.dart
+++ lib/stopviewscreen.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:travigo/stop.dart';
+import 'dart:convert';
+// ignore: depend_on_referenced_packages
+import 'package:http/http.dart' as http;
 
-class StopViewScreen extends StatelessWidget {
+class DepartureBoard {
+  String? destinationDisplay;
+  String? time;
+  String? type;
+
+  DepartureBoard(
+      {this.destinationDisplay,
+      this.time,
+      this.type});
+
+  DepartureBoard.fromJson(Map<String, dynamic> json) {
+    destinationDisplay = json['DestinationDisplay'];
+    time = json['Time'];
+    type = json['Type'];
+  }
+}
+
+class StopViewScreen extends StatefulWidget {
   const StopViewScreen({super.key, required this.stop});
 
   final Stop stop;
+
+  @override
+  State<StopViewScreen> createState() => _StopViewScreenState(stop);
+}
+
+class _StopViewScreenState extends State<StopViewScreen> {
+  _StopViewScreenState(this.stop);
+  late Future<List<DepartureBoard>> futureDepartureBoards;
+
+  final Stop stop;
+
+  @override
+  void initState() {
+    super.initState();
+    futureDepartureBoards = fetchStopDepartures(stop);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,14 +48,66 @@ class StopViewScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(stop.primaryName!),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
-        ),
+      body: FutureBuilder<List<DepartureBoard>>(
+        future: futureDepartureBoards,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                DepartureBoard departure = snapshot.data![index];
+
+                return ListTile(
+                  // leading: stop.getIcon(),
+                  title: Text(departure.destinationDisplay!, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                  subtitle: Text("${departure.time!} ${departure.type!}", style: const TextStyle(fontSize: 12)),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+
+          // By default, show a loading spinner.
+          return const CircularProgressIndicator();
+        },
       ),
+      // body: Center(
+      //   child: FutureBuilder<List<DepartureBoard>>(
+      //     future: futureDepartureBoards,
+      //     builder: (context, snapshot) {
+      //       if (snapshot.hasData) {
+      //         return Column(children: [ for (var departure in snapshot.data!) Card(
+      //           child: ListTile(
+      //             // leading: stop.getIcon(),
+      //             title: Text(departure.destinationDisplay!, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+      //             subtitle: Text("${departure.time} ${departure.type}", style: const TextStyle(fontSize: 12)),
+      //           ),
+      //         ) ]);
+      //       } else if (snapshot.hasError) {
+      //         return Text('${snapshot.error}');
+      //       }
+
+      //       // By default, show a loading spinner.
+      //       return const CircularProgressIndicator();
+      //     },
+      //   ),
+      // ),
     );
+  }
+}
+Future<List<DepartureBoard>> fetchStopDepartures(Stop stop) async {
+  final response = await http
+      .get(Uri.parse("https://api.travigo.app/core/stops/${stop.primaryIdentifier}/departures?count=25"));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    Iterable l = json.decode(response.body);
+    return List<DepartureBoard>.from(l.map((model)=> DepartureBoard.fromJson(model)));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load DepartureBoards');
   }
 }
